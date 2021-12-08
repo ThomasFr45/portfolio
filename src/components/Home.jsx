8d27d3f34ebf2c91ad67ffd1bfc42e49482d4d2e
thomas Fran <thomasfran1935@icloud.com>
2021-12-08 16:07:18 +0100

    Fixed button size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <p className='home-text'>Je suis Thomas FRANCOIS, Développeur Web FullStack</p>
       <p className='home-text'>Mon but : Apprendre un maximum de choses, accumuler de l'expérience.</p>
       <p className='home-text'>Pour en savoir plus, vous pouvez consulter mon profil ainsi que les projets sur lesquels j'ai travaillé.</p>
-      <Link to="contact"><div className='home-button'><p>Contacter-moi !</p></div></Link>
+      <Link to="contact"><div className='home-button'><p>Contactez-moi !</p></div></Link>
       </div>
     </div>
   )
